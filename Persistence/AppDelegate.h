eb642a044716200b2a18ce87e4c9082d0fcd3d33
Diego Mazzone <mazzone.diego@gmail.com>
2015-04-16 15:46:00 -0300

    Initial Commit

---
+++ Persistence/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Persistence
+//
+//  Created by Diego Mazzone on 16/4/15.
+//  Copyright (c) 2015 Mundo Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Persistence/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Persistence
+//
+//  Created by Diego Mazzone on 16/4/15.
+//  Copyright (c) 2015 Mundo Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
